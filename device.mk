2744e0759557a9d1be3c87c913f8780f6171e6c3
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-11-29 14:26:07 -0600

    mako: add facelock props
    Change-Id: I6666be9d8e1360858faaecd1fc2a2174161e4901

--- device.mk
+++ device.mk
@@ -261,6 +261,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qc.sensors.wl_dis=true \
 	ro.qualcomm.sensors.smd=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
