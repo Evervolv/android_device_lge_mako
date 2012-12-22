5ebd997510900d9eec1171a5848cce409442bf39
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-21 20:57:50 -0600

    mako: use updated caf display variant
    Change-Id: I997db734e4271d51b2db4ca6bd728fc8b32cd6fc

--- device.mk
+++ device.mk
@@ -147,8 +147,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.call_ring.multiple=0
 
-#Upto 3 layers can go through overlays
-PRODUCT_PROPERTY_OVERRIDES += debug.mdpcomp.maxlayer=3
+# enable overlay pipes
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.hwc.mdpcomp.enable=1
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
